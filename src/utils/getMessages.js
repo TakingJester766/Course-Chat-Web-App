7502f4c6691e27a8376d4b14098039ebe175dd88
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-06-02 12:03:23 -0400

    adjusted styling

--- src/utils/getMessages.js
+++ src/utils/getMessages.js
@@ -2,6 +2,6 @@ import { collection, query, orderBy, limit } from "firebase/firestore";
 import { firestore } from '../configurations/firebase.js';
 
 const messagesRef = collection(firestore, 'messages');
-const messagesQuery = query(messagesRef, orderBy('createdAt'), limit(25));
+const messagesQuery = query(messagesRef, orderBy('createdAt'), limit(100));
 
 export { messagesRef, messagesQuery };
