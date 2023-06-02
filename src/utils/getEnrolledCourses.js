fe00aa21e0a04ee43c0a61e4016f3680aad8cce0
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-06-02 11:39:33 -0400

    Successfully fetch courses subcollection

--- src/utils/getEnrolledCourses.js
+++ src/utils/getEnrolledCourses.js
@@ -1,26 +1,18 @@
 import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { firestore } from '../configurations/firebase.js';
-import { getAuth } from "firebase/auth";
+import getUid from './getUid.js';
 
-//const { uid } = auth.currentUser;
+//users collection, then uid document, then courses subcollection. make this an async function
+const getEnrolledCourses = async () => {
+  const uid = await getUid();
+  const queryGetEnrolled = await getDocs(collection(firestore, 'users', uid, 'courses'));
+  const enrolledCourses = [];
+  queryGetEnrolled.forEach((doc) => {
+    enrolledCourses.push(doc.data());
+  }
+  ); 
+  return enrolledCourses;
+}
 
-const auth = getAuth();
 
-const sfRef = firestore.collection('users').doc();
-const collections = await sfRef.listCollections();
-collections.forEach(collection => {
-  console.log('Found subcollection with id:', collection.id);
-});
-
-
-
-/*
-WORKS TO GET USERS
-
-const querySnapshot = await getDocs(collection(firestore, "users"));
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});
-
-*/
+export default getEnrolledCourses;
